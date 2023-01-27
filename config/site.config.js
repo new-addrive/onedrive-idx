bcf205fc1ecabb942762a13936829b3a0bb96094
new-addrive <121278276+new-addrive@users.noreply.github.com>
2023-01-27 22:45:55 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -45,7 +45,7 @@ module.exports = {
   protectedRoutes: ['/Private'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: 'mailto:admin@addrive.org',
+  email: 'mailto:admin@addrive.tech',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
